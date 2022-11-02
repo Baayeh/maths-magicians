ef6e0a175ccdbd403c70bdaa04ebb1e65d04c053
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-11-02 09:41:09 +0000

    Style Quote Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 const Home = () => (
-  <section className='mt-10 p-6 shadow-inner bg-white text-primary rounded-full text-center h-72 flex flex-col justify-center gap-2'>
+  <section className='mt-10 py-8 px-3 md:w-[80%] md:px-20 md:mx-auto shadow-inner bg-white text-primary rounded-xl md:rounded-full text-center h-80 flex flex-col md:pt-24 gap-2'>
     <h1 className='text-3xl'>
       Welcome to <span className='font-extrabold'>MathGuru</span>
     </h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quisquam soluta qui sint nam ut, deserunt atque ratione, quia,
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis corrupti assumenda eos veritatis distinctio dolorum, incidunt ex saepe eaque quo nesciunt fugit rerum soluta, voluptas sit nulla aperiam, exercitationem quisquam!
     </p>
   </section>
 );
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,9 +1,37 @@
-import React from 'react'
+/* eslint-disable react/self-closing-comp */
+import React, { useState, useEffect } from 'react'
+import quotes from '../components/QuotesArray';
 
-const Quote = () => (
-  <>
-    <h2>Quote</h2>  
-  </>
-);
+const Quote = () => {
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  
+
+  useEffect(() => {
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(random.text);
+    setAuthor(random.author);
+  }, []);
+
+  const changeQuote = () => {
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(random.text);
+    setAuthor(random.author);
+  }
+
+  return (
+    <section className='mt-10 py-8 px-3 md:w-[80%] md:px-20 md:py-24 md:mx-auto shadow-inner bg-white text-primary rounded-xl md:rounded-full text-center h-80 md:h-72 flex flex-col justify-center gap-2'>
+    <h1 className='hidden md:block md:text-3xl font-extrabold underline decoration-secondary'>
+      Quote from Famous People
+    </h1>
+    <q className='quote text-xl md:text-2xl'>{quote}</q>
+    <p className='author font-extrabold text-lg md:text-xl'>~ {author}</p>
+    <div className="quote-action mt-5">
+      <button type='button' className='bg-slate-400 text-sm p-3 rounded-full uppercase font-bold hover:bg-slate-500 transition ease-out duration-300' onClick={changeQuote}>New Quote</button>
+    </div>
+  </section>
+  );
+  
+};
 
 export default Quote
